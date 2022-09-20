9e8cd1c869568401bb9a5b64a6a9e0ec91410316
Massimiliano Garzoni di Adorgnano <39051658+MGarzoni@users.noreply.github.com>
2022-09-20 17:17:37 +0200

    Add files via upload

---
+++ db_stats.py
@@ -0,0 +1,23 @@
+'''
+Use output of filter (csv file) to find out:
+    (a) what is the average shape in the database (in terms of vertex and face counts)
+    (b) if there are significant outliers from this average (e.g. shapes having many, or few, vertices or cells)
+    best way to do this is to show a histogram counting how many shapes are in the database for every range of the property of interest
+    (e.g., number of vertices, number of faces, shape class)
+
+Use the viewer constructed in step 1 to show an average shape and a few such outliers (if any)
+'''
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# import and load analysis data
+analysis = './psb_analysis.csv'
+analysis_in = pd.read_csv(analysis)
+
+# inspect dataframe
+analysis_in.describe()
+analysis_in.info()
+
+# plotting
+plt.hist(analysis_in)
