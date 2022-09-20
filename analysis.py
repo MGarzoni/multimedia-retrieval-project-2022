0ed1dc622ba5b0934dca40076c866eee3d11a31c
Massimiliano Garzoni di Adorgnano <39051658+MGarzoni@users.noreply.github.com>
2022-09-20 09:05:21 +0200

    Add files via upload

--- analysis.py
+++ analysis.py
@@ -1,6 +1,6 @@
 import os
 import trimesh
-import random
+import pandas as pd
 
 # DB PATHS
 PRINCETON_PATH = "./princeton-labeled-db/"
@@ -14,20 +14,24 @@ def inspect_db(db_path):
     db_info = {}
 
     # for each category (folder=dirname) create an empty list as value associated with that category
-    for dirname in os.listdir("./princeton-labeled-db/"):
+    for dirname in os.listdir(db_path):
         db_info[dirname] = {}
 
         # for each object (filename)
-        for filename in os.listdir(os.path.join("./princeton-labeled-db/", dirname)):
+        for filename in os.listdir(os.path.join(db_path, dirname)):
 
             # consider only 3D mesh files and use trimesh to load the mesh
             if filename.endswith(('.ply', '.obj', '.off')):
                 db_info[dirname].update({filename: {}})
-                mesh = trimesh.load(os.path.join("./princeton-labeled-db/", dirname, filename))
+                mesh = trimesh.load(os.path.join(db_path, dirname, filename))
 
                 # save a dict containing all the desired attributes
                 db_info[dirname][filename].update({'num_faces': len(mesh.faces), 'num_vertices': len(mesh.vertices), 'faces_type': 'triangles', 'axis_aligned_bounding_box': mesh.bounding_box.extents})
 
     return db_info
 
-inspect_db(PSB_PATH)
+out_dict = inspect_db(PSB_PATH)
+
+# save to csv
+output = pd.DataFrame.from_dict(out_dict)
+output.to_csv('psb_analysis.csv')
