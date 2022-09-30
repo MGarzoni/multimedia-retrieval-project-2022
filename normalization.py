319a22802df7b02fb7c597bc1296510a99f48a26
Massimiliano Garzoni di Adorgnano <massimiliano.garzoni@gmail.com>
2022-09-30 17:03:54 +0200

    added attempt to normalization_pipeline() function

--- normalization.py
+++ normalization.py
@@ -6,9 +6,11 @@
 '''
 
 # imports
+import csv
 import trimesh
 import os
 import numpy as np
+import pandas as pd
 
 # utils
 from utils import *
@@ -39,16 +41,67 @@ mesh = scale_to_unit(mesh)
 save_mesh_png(mesh, "scaled", corners = corners)
 scaled_mesh = mesh.copy()
 
-# PCA 
+# PCA
 eigenvalues, eigenvectors = pca_eigenvalues_eigenvectors(mesh)
-print("==> eigenvalues for (x, y, z)")
-print(eigenvalues)
-print("\n==> eigenvectors")
-print(eigenvectors)
-
-# # before and after pictures
-# before_after(original_mesh, mesh, corners)
+print(f"=> eigenvalues for (x, y, z)\n{eigenvalues}")
+print(f"=> eigenvectors\n{eigenvectors}")
 
 # print stats to demonstrate changes
-print("Original barrycenter: {}\nOriginal bounds:\n{}".format(original_mesh.centroid, original_mesh.bounds) )
-print("\nNEW barrycenter: {}\nNEW bounds:\n{}".format(mesh.centroid, mesh.bounds) )
+print(f"Original barycenter: {original_mesh.centroid}\nOriginal bounds:\n{original_mesh.bounds}")
+print(f"NEW barycenter: {mesh.centroid}\nNEW bounds:\n{mesh.bounds}")
+
+
+'''
+function pipeline( path, out_dir, display_meshes=False ):
+
+    define output dictionary holding info about updated values to add to newshapes.csv
+    
+    read original_csv file
+
+    check if path is outlier (possibly treat this exception):
+        run java subdivider and get refined mesh (here num_vertices changes value)
+        update num_vertices column
+
+    load shape from path
+
+    translate mesh to origin (with center_at_origin function from utils) (here bounds value changes)
+    scale to cube vector (with scale_to_unit function from utils) (here bounds value changes)
+    compute eigenvalues and eigenvectors
+    apply pca on them (with pca function from utils) (here value changes)
+
+    call function to extract attributes and add them to output_dictionary
+
+    return output_dictionary
+'''
+
+def normalization_pipeline(path, csv_file, out_dir, display_mesh=False):
+    
+#     check if path is outlier (possibly treat this exception):
+#         run java subdivider and get refined mesh (here num_vertices changes value)
+#         update num_vertices column
+
+    # load shape from path
+    mesh = trimesh.load(path)
+
+    # translate mesh to origin (with center_at_origin function from utils) (here bounds value changes)
+    mesh = center_at_origin(mesh)
+    translated_mesh = mesh.copy()
+
+    # scale to cube vector (with scale_to_unit function from utils) (here bounds value changes)
+    mesh = scale_to_unit(mesh)
+    scaled_mesh = mesh.copy()
+
+    # get eigenvalues and eigenvectors (with pca function from utils) (here value changes)
+    eigenvalues, eigenvectors = pca_eigenvalues_eigenvectors(mesh)
+    print(f"=> eigenvalues for (x, y, z)\n{eigenvalues}")
+    print(f"=> eigenvectors\n{eigenvectors}")
+
+    # print stats to demonstrate changes
+    print(f"Original barycenter: {original_mesh.centroid}\nOriginal bounds:\n{original_mesh.bounds}")
+    print(f"NEW barycenter: {mesh.centroid}\nNEW bounds:\n{mesh.bounds}")
+
+    # call function to extract attributes and add them to output_dictionary
+    out_dict = extract_attributes(path, outliers_range=range(3500))
+
+    return out_dict
+
